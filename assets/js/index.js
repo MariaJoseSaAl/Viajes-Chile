f872dd292c220c5a9a18354b64af761b1ae1b975
Maria Jose <ma.josesalazaralfonzo@gmail.com>
2024-04-10 21:53:34 -0400

    cambio

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+var enviar = document.getElementById("enviar")
+
+$(document).ready(function() {
+    $("#enviar").click(function() {
+        alert("El correo fue enviado correctamente...");
+    });
+});
